58f6f34523175b60097c3917008a099cb1f3d0f5
Douglas Molina <douglas.molina@intertecintl.com>
2017-02-18 15:26:54 -0600

    adding public folder and express server

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
-module.exports = {
+var webpack = require('webpack');
+var path = require('path');
+var envFile = require('node-env-file');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+try {
+  envFile(path.join(__dirname, 'config/' + process.env.NODE_ENV + '.env'));
+} catch (e) {
+  console.log('Error loading Environment file. Details: '+ e);
+}
+
+
+const config = {
   entry: [
     './src/index.js'
   ],
   output: {
     path: __dirname,
-    publicPath: '/',
-    filename: 'bundle.js'
+    publicPath: '/public',
+    filename: './public/bundle.js'
   },
   module: {
     loaders: [{
@@ -16,6 +29,18 @@ module.exports = {
       }
     }]
   },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+        API_KEY: JSON.stringify(process.env.API_KEY),
+        AUTH_DOMAIN: JSON.stringify(process.env.AUTH_DOMAIN),
+        DATABASE_URL: JSON.stringify(process.env.DATABASE_URL),
+        STORAGE_BUCKET: JSON.stringify(process.env.STORAGE_BUCKET),
+        MESSAGING_SENDER_ID: JSON.stringify(process.env.MESSAGING_SENDER_ID)
+      }
+    })
+  ],
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
@@ -24,3 +49,6 @@ module.exports = {
     contentBase: './'
   }
 };
+
+
+module.exports = config;
