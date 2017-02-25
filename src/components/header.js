ae1a49aeaad58ba6089ca83c3a348ea4a6a76000
Douglas Molina <douglas.molina@intertecintl.com>
2017-02-25 16:42:06 -0600

    merged

--- src/components/header.js
+++ src/components/header.js
@@ -5,27 +5,34 @@ import { Link } from 'react-router';
 export class Header extends Component {
 	renderLinks() {
 		const authenticated = this.props.authenticated;
+		let links = [];
+		
 		if (authenticated) {
-			return (
+			links = ([
 
-
-			<li className="nav-item" >
-				<Link className="nav-link" to="/signout">Sign Out</Link>
-			</li>
-
-
-		);
+				<li className="nav-item" key={3}>
+					<Link className="nav-link" to="creategroupchat">Create a GroupChat</Link>
+				</li>,
+				<li className="nav-item" key={4}>
+					<Link className="nav-link" to="findgroupchat">Find a GroupChat</Link>
+				</li>,
+				<li className="nav-item" >
+					<Link className="nav-link" to="/signout">Sign Out</Link>
+				</li>
+			]);
 		} else {
 			//show sign in and sign up links
-			return [
-			<li className="nav-item" key={1}>
-				<Link className="nav-link" to="/signin">Sign In</Link>
-			</li>,
-			<li className="nav-item" key={2}>
-				<Link className="nav-link" to="/signup">Sign Up</Link>
-			</li>
-			];
+			links = ([
+				<li className="nav-item" key={1}>
+					<Link className="nav-link" to="/signin">Sign In</Link>
+				</li>,
+				<li className="nav-item" key={2}>
+					<Link className="nav-link" to="/signup">Sign Up</Link>
+				</li>
+			]);
 		}
+
+		return (links);
 	}
 
 	render() {
@@ -34,13 +41,6 @@ export class Header extends Component {
 				<Link to="/" className="navbar-brand">Welcome to Group Chat</Link>
 
 				<ul className="nav navbar-nav" id="nav1">
-				<li className="nav-item" key={3}>
-					<Link className="nav-link" to="creategroupchat">Create a GroupChat</Link>
-				</li>
-				<li className="nav-item" key={4}>
-					<Link className="nav-link" to="findgroupchat">Find a GroupChat</Link>
-				</li>
-
 					{this.renderLinks()}
 				</ul>
 
