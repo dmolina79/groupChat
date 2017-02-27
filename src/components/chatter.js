76b29160340c495ef9ec318d7f0084da2692b915
pamela <laitanop@yahoo.com>
2017-02-27 08:26:39 -0600

    css

--- src/components/chatter.js
+++ src/components/chatter.js
@@ -7,7 +7,7 @@ class Chatter extends Component{
     return (
       <div>
 
-
+<form action="">
       <div className="panel panel-default" id="chatter">
 
         <div className="chatHeader">
@@ -15,13 +15,15 @@ class Chatter extends Component{
         </div>
 
         <div className="chatMessage"></div>
+        <textarea id="menssge"></textarea>
 
         <div className="chatBottom">
+
         <input type="text" className="form-control" placeholder="Message" aria-describedby="sizing-addon1"  id="chatterName"/>
         </div>
 
         </div>
-
+</form>
 
       </div>
     );
