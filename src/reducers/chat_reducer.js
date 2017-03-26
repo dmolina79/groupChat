15f19ce16cdf200de67060c5bda6aedc55ca4776
Douglas Molina <douglas.molina@intertecintl.com>
2017-03-26 17:53:31 -0600

    added chat updates in real time

--- src/reducers/chat_reducer.js
+++ src/reducers/chat_reducer.js
@@ -1,19 +1,11 @@
 import
 {
-  CHAT_LOADING,
-  CHAT_LOADED,
+	CHAT_MSGS_LOADED,
   POST_MSG,
-  POST_MSG_FAIL,
-  POST_MSG_LOADING
 } from '../actions/types';
 
 const INITIAL_STATE = {
   loading: true,
-  groupChatInfo: {
-    name: '',
-    channels: [],
-    selectedChannel: 'default'
-  },
   chatInfo: {
     messages: []
   }
@@ -21,25 +13,24 @@ const INITIAL_STATE = {
 
 export default function (state = INITIAL_STATE, action) {
 	switch (action.type) {
-    case CHAT_LOADING:
-      return { ...state, ...INITIAL_STATE, loading: true };
-    case CHAT_LOADED: {
-      const { groupChatInfo, chatInfo } = action.payload;
+    // case CHAT_LOADING:
+    //   return { ...state, ...INITIAL_STATE, loading: true };
+    case CHAT_MSGS_LOADED: {
+      const { chatInfo } = action.payload;
       return {
         ...state,
         ...INITIAL_STATE,
         loading: false,
-        groupChatInfo,
         chatInfo
       };
     }
-    case POST_MSG: {
-      const { chatInfo } = state;
-      const newMessages = [...chatInfo.messages, action.payload];
-      const newChatInfo = { ...chatInfo, messages: newMessages };
-
-      return { ...state, chatInfo: newChatInfo };
-    }
+    // case POST_MSG: {
+    //   const { chatInfo } = state;
+    //   const newMessages = [...chatInfo.messages, action.payload];
+    //   const newChatInfo = { ...chatInfo, messages: newMessages };
+    //
+    //   return { ...state, chatInfo: newChatInfo };
+    // }
 
 		default:
       return state;
