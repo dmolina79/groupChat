dce1ae6175abce26a1e16e8fd0b5bce3735abeb7
pamela <laitanop@yahoo.com>
2017-05-26 15:26:44 -0600

    img & package.json

--- src/components/landing.js
+++ src/components/landing.js
@@ -7,10 +7,10 @@ const Landing = () => {
 			<div className='text-center'>
 				<h1 className='display-3'><strong>Welcome to GroupChat!</strong></h1>
 				<h4 className='lead'>A showcase web app chat made with modern technology</h4>
-				<img className="logo" src="img/react-blue.png" alt="https://facebook.github.io/react/" title="React" />
+				<img className="logo" src="img/React-blue.png" alt="https://facebook.github.io/react/" title="React" />
 				<img className="logo" src="img/redux.png" alt="http://redux.js.org" title="Redux" />
 				<img className="logo" src="img/firebase.png" alt="https://firebase.google.com" title="Firebase" />
-				<img className="logo" src="img/bootstrap.png" alt="http://getbootstrap.com" title="Bootstrap" />				
+				<img className="logo" src="img/bootstrap.png" alt="http://getbootstrap.com" title="Bootstrap" />
 			</div>
 		</section>
 	);
